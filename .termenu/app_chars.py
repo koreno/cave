df57c9799e9d476063bfec9a991cca9234543306
Ofer Koren <koreno@gmail.com>
2019-09-09 08:51:37 +0300

    added termenu

---
+++ .termenu/app_chars.py
@@ -0,0 +1,13 @@
+
+# WHITE<<termenu>> has created for you this default configuration file.
+# You can modify it to control which glyphs are used in termenu apps, to improve the readability
+# and usuability of these apps. This depends on the terminal you use.
+# This could be helpful: CYAN<<http://xahlee.info/comp/unicode_geometric_shapes.html>>
+
+SCROLL_UP_MARKER = "🢁"  # consider 🢁
+SCROLL_DOWN_MARKER = "🢃"  # consider 🢃
+ACTIVE_ITEM_MARKER = " WHITE@{🞂}@"  # consider 🞂
+SELECTED_ITEM_MARKER = "CYAN@{⚫}@"  # consider ⚫
+SELECTABLE_ITEM_MARKER = "⚪"  # consider ⚪
+CONTINUATION_SUFFIX = "DARK_RED@{↩}@"  # for when a line overflows
+CONTINUATION_PREFIX = "DARK_RED@{↪}@"  # for when a line overflows
